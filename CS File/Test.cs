4efcb92728e21d41d67c2931a56821c6852b9611
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-09-23 09:52:19 +0800

    Add files via upload

---
+++ CS File/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cshape_Socket
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            BlockingSocket blockingSocket = new BlockingSocket("localhost",5555);
+            blockingSocket.Sendmessage("Hello Java Server I'm C# Client");
+            Thread.Sleep(5000);
+            blockingSocket.Close();
+        }
+    }
+}
